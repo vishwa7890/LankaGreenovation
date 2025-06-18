9aa81981d5d73ca3e85685aa960b0caa1eed9352
Aathithya13 <aathi22004@gmail.com>
2025-06-18 21:00:06 +0530

    replaced

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  server:{
+    port:3000
   },
-  css: {
-    preprocessorOptions: {
-      css: {},
+  css:{
+    preprocessorOptions:{
+      css:{},
     },
   },
-  // ✅ This is the key addition for GitHub Pages
-  base: '/Lanka-Greenovation/',
 });
